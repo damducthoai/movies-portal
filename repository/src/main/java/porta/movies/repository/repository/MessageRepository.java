229663813cceb2ceedfbec46852972d200c41799
root <damducthoai@gmail.com>
2017-09-24 22:23:32 +0700

    restructure project

--- repository/src/main/java/porta/movies/repository/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package porta.movies.repository;
-
-import com.butchjgo.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class MessageRepository extends JpaRepository<Message, Integer> {
-}
